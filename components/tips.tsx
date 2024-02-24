9a4f454a89409202ae6a55010beb15a2c41e4907
Yoseph <yosephten@gmail.com>
2024-02-24 22:45:37 +0300

    `fix`: corrected tips

--- components/tips.tsx
+++ components/tips.tsx
@@ -10,19 +10,27 @@ import {
 export default function Tips() {
   return (
     <Carousel
-      className="w-full max-w-xs absolute bottom-10"
+      className="w-full max-w-xs"
       orientation="vertical"
       opts={{
         align: "start",
       }}
     >
-      <CarouselContent className="-mt-1 h-[100px]">
+      <CarouselContent className="-mt-1 h-[250px]">
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index} className="pt-1 md:basis-1/2">
             <div className="py-1">
               <Card>
-                <CardContent className="flex items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
+                <CardContent className="flex flex-col items-start justify-center p-6">
+                  <span className="text-sm font-semibold text-start">
+                    Tip #{index + 1}
+                  </span>
+                  <span className="text-sm font-normal">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Eveniet, illo. Nemo, veniam officiis. Soluta, minima. Lorem,
+                    ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+                    illo. Nemo, veniam officiis. Soluta, minima.
+                  </span>
                 </CardContent>
               </Card>
             </div>
