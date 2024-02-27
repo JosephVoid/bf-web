ba275a61419a29a2be5937c4d1c190eb5d60ce69
Yoseph <yosephten@gmail.com>
2024-02-27 22:49:21 +0300

    `feat`: added offer list component

---
+++ components/offer-list.tsx
@@ -0,0 +1,44 @@
+import { Card } from "./ui/card";
+import Image from "next/image";
+
+function Offer() {
+  return (
+    <Card className="p-4 mb-5 flex flex-col">
+      <div className="flex justify-start">
+        <Image
+          src={"/sample.jpg"}
+          height={150}
+          width={150}
+          alt="desire"
+          className="rounded-md mr-5"
+        />
+        <div className="flex flex-col justify-between">
+          <div>
+            <small className="text-sm font-normal line-clamp-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quae
+              recusandae quaerat commodi vero tenetur possimus quos distinctio
+              fugit sed dolor sapiente architecto, quidem error est beatae,
+              alias corporis magnam? recusandae quaerat commodi vero tenetur
+              possimus quos distinctio fugit sed dolor sapiente architecto,
+              quidem error est beatae, alias corporis magnam?
+            </small>
+          </div>
+          <p className="text-sm">
+            Offering for
+            <b> 800 Br</b>
+          </p>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default function OfferList() {
+  return (
+    <div>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <Offer key={index} />
+      ))}
+    </div>
+  );
+}
