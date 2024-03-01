3fa33b5a91fa2ae2e5bce1f0194505ffafd7294f
Yoseph <yosephten@gmail.com>
2024-03-01 15:15:08 +0300

    `feat`: added all the components on the page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,18 @@
 import EditProfileForm from "@/components/edit-profile-form";
+import SetAlert from "@/components/set-alert";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import UserDesireList from "@/components/user-desires";
+import UserOfferList from "@/components/user-offers";
 import { Pencil1Icon } from "@radix-ui/react-icons";
 
 export default function ProfilePage() {
   return (
     <div className="m-3 p-4">
       <EditProfileForm />
+      <SetAlert />
+      <UserDesireList />
+      <UserOfferList />
     </div>
   );
 }
