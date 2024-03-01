f5e50f12f69c5cb13c713bbeb19590492e3198de
Yoseph <yosephten@gmail.com>
2024-03-01 15:13:02 +0300

    `feat`: finished edit profile form

--- components/edit-profile-form.tsx
+++ components/edit-profile-form.tsx
@@ -12,15 +12,17 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
+import { Label } from "./ui/label";
+import { AvatarIcon, Pencil2Icon } from "@radix-ui/react-icons";
 
 export default function EditProfileForm() {
   const form = useForm();
   return (
     <>
       <h2 className="text-2xl font-medium mb-3"> Edit Profile</h2>
-      <div className="rounded-md border-[1px] p-4">
+      <div className="rounded-md border-[1px] p-4 mb-8">
         <Form {...form}>
-          <div className="flex flex-col">
+          <div className="flex flex-col relative">
             <div className="mb-4 flex space-x-5">
               <FormField
                 control={form.control}
@@ -105,6 +107,38 @@ export default function EditProfileForm() {
                 )}
               />
             </div>
+            <div className="mb-4 flex">
+              <FormField
+                control={form.control}
+                name="desc"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col w-full">
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      {/* <Input placeholder="Old Password" {...field} /> */}
+                      <textarea
+                        placeholder="Tell us about you"
+                        {...field}
+                        rows={6}
+                        className="p-2 text-sm border-[1px] rounded-md focus-visible:outline-none focus:outline-2 focus:outline"
+                      ></textarea>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mb-4 flex">
+              <div className="w-1/2">
+                <Label htmlFor="picture">Profile Picture</Label>
+                <Input type="file" id="picture" />
+              </div>
+              <div className="flex justify-center items-center w-1/2">
+                <AvatarIcon width={50} height={50} />
+              </div>
+            </div>
+            <Button className="w-1/3">Update</Button>
+            <Pencil2Icon className="absolute top-2 right-2 scale-150 cursor-pointer" />
           </div>
         </Form>
       </div>
