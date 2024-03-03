8f6a0163b452ea8954d75dd6b27009bf93439b81
Yoseph <yosephten@gmail.com>
2024-03-03 15:17:09 +0300

    `fix`: form alignment

--- app/[desire_id]/make-an-offer/page.tsx
+++ app/[desire_id]/make-an-offer/page.tsx
@@ -2,7 +2,7 @@ import MakeAnOfferForm from "@/components/make-an-offer-form";
 
 export default function MakeAnOffer() {
   return (
-    <div className="mt-8">
+    <div className="mt-8 border-l-4 pl-8">
       <MakeAnOfferForm />
     </div>
   );
