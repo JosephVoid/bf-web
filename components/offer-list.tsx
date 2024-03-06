0f0d58acdce35011770140275dce00c004d3f6de
Yoseph <yosephten@gmail.com>
2024-03-06 16:29:44 +0300

    `feat`: integrated server actions with components [offers]

--- components/offer-list.tsx
+++ components/offer-list.tsx
@@ -1,37 +1,36 @@
 "use client";
 
+import { IOffer } from "@/lib/types";
 import { Card } from "./ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { fetchOffers } from "@/lib/actions/fetch/offer.fetch";
 
-export function Offer() {
+export function Offer({ prop }: { prop: IOffer }) {
   const current_path = usePathname();
   return (
     <Link href={`${current_path}/0`}>
       <Card className="p-4 mb-5 flex flex-col">
         <div className="flex justify-start">
-          <Image
-            src={"/sample.jpg"}
-            height={150}
-            width={150}
-            alt="desire"
-            className="rounded-md mr-5"
-          />
+          {prop.picture && (
+            <Image
+              src={prop.picture ?? ""}
+              height={150}
+              width={150}
+              alt="desire"
+              className="rounded-md mr-5"
+            />
+          )}
           <div className="flex flex-col justify-between">
             <div>
               <small className="text-sm font-normal line-clamp-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-                quae recusandae quaerat commodi vero tenetur possimus quos
-                distinctio fugit sed dolor sapiente architecto, quidem error est
-                beatae, alias corporis magnam? recusandae quaerat commodi vero
-                tenetur possimus quos distinctio fugit sed dolor sapiente
-                architecto, quidem error est beatae, alias corporis magnam?
+                {prop.description}
               </small>
             </div>
             <p className="text-sm">
               Offering for
-              <b> 800 Br</b>
+              <b> {prop.price} Br</b>
             </p>
           </div>
         </div>
@@ -40,11 +39,12 @@ export function Offer() {
   );
 }
 
-export default function OfferList() {
+export default async function OfferList() {
+  const offerList = await fetchOffers(0);
   return (
     <div>
-      {Array.from({ length: 3 }).map((_, index) => (
-        <Offer key={index} />
+      {offerList.map((offer: IOffer, index: number) => (
+        <Offer key={index} prop={offer} />
       ))}
     </div>
   );
--- components/user-offers.tsx
+++ components/user-offers.tsx
@@ -1,11 +1,14 @@
+import { fetchOffers, fetchUserOffers } from "@/lib/actions/fetch/offer.fetch";
 import { Offer } from "./offer-list";
+import { IOffer } from "@/lib/types";
 
-export default function UserOfferList() {
+export default async function UserOfferList() {
+  const offerList = await fetchUserOffers(0);
   return (
     <div>
       <h2 className="text-2xl font-medium mb-3">You Offered</h2>
-      {Array.from({ length: 2 }).map((_, index) => (
-        <Offer key={index} />
+      {offerList.map((offer: IOffer, index) => (
+        <Offer key={index} prop={offer} />
       ))}
     </div>
   );
