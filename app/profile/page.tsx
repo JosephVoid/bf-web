57381db90623027907c9d0c53c6a50e50c7f9cd2
Yoseph <yosephten@gmail.com>
2024-03-07 19:51:04 +0300

    `feat`: integrated stub with profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,13 +4,15 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import UserDesireList from "@/components/user-desires";
 import UserOfferList from "@/components/user-offers";
+import { fetchUserProfile } from "@/lib/actions/fetch/user.fetch";
 import { Pencil1Icon } from "@radix-ui/react-icons";
 import { Suspense } from "react";
 
-export default function ProfilePage() {
+export default async function ProfilePage() {
+  const user = await fetchUserProfile(0);
   return (
     <div className="m-3 p-4">
-      <EditProfileForm />
+      <EditProfileForm prop={user} />
       <SetAlert />
       <Suspense fallback={<>Loading...</>}>
         <UserDesireList />
--- lib/actions/fetch/user.fetch.ts
+++ lib/actions/fetch/user.fetch.ts
@@ -1,3 +1,9 @@
-export async function fetchUserProfile(userId: number) {}
+import { IUser } from "@/lib/types";
+import mockUser from "@/lib/mock/users.json";
+
+export async function fetchUserProfile(userId: number): Promise<IUser> {
+  const user = mockUser[0] as unknown;
+  return <IUser>user;
+}
 
 export async function fetchUserAlerts(userId: number) {}
