79d7ec8942225ad97223739909b55b9d0a1986ee
Yoseph <yosephten@gmail.com>
2024-04-03 00:24:31 +0300

    `test`: added new testing param

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from "vitest";
-import API from "@/lib/api";
+import { AuthAPI, CoreAPI } from "@/lib/api";
 import { AxiosError } from "axios";
 
 test("Sign In Negative Test", async () => {
   try {
-    const resp = await API.signIn({ username: "", password: "" });
+    const resp = await AuthAPI.signIn({ email: "", password: "" });
   } catch (error: unknown) {
     const axiosErr = <AxiosError>error;
     expect(axiosErr.response?.status).toBe(401);
@@ -13,8 +13,8 @@ test("Sign In Negative Test", async () => {
 
 test("Sign In Test", async () => {
   try {
-    const resp = await API.signIn({
-      username: "yosephten@gmail.com",
+    const resp = await AuthAPI.signIn({
+      email: "yosephten@gmail.com",
       password: "12345678",
     });
     expect(resp.status).toBe(200);
@@ -22,4 +22,17 @@ test("Sign In Test", async () => {
     expect(resp.data).not.toBe(undefined);
     expect(resp.data).not.toBe(null);
   } catch (error: unknown) {}
+});
+
+test("Sign In and View", async () => {
+  const authResp = await AuthAPI.signIn({
+    email: "yosephten@gmail.com",
+    password: "12345678",
+  });
+  expect(authResp.status).toBe(200);
+  CoreAPI.setAuthToken(authResp.data);
+  const coreResp = await CoreAPI.viewItem(
+    "95999305-ea0a-4e29-ba89-1527b111893b"
+  );
+  expect(coreResp.status).toBe(200);
 });
