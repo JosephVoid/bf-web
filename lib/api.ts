502f16b4a8bd3ecb5f5ff11ee6a44d1b66180657
Yoseph <yosephten@gmail.com>
2024-04-03 00:24:50 +0300

    `feat`: implemented API stubs

--- lib/api.ts
+++ lib/api.ts
@@ -1,33 +1,133 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { IFilterParams } from "./types";
 
 class APIStub {
-  private static _instance: any;
-
-  private baseURL: string = process.env.NEXT_PUBLIC_BASE_URL ?? "";
-
-  set setAuthToken(token: string) {
+  setAuthToken(token: string | undefined) {
     this.axios.defaults.headers.common.Authorization = token;
   }
 
-  private axios: AxiosInstance = axios.create({
-    baseURL: this.baseURL,
-  });
+  protected axios: AxiosInstance;
 
-  constructor() {
-    if (APIStub._instance) {
-      return APIStub._instance;
-    }
-    APIStub._instance = this;
-  }
-
-  async signIn(body: any): Promise<AxiosResponse> {
-    return this.axios.post("/auth/", body);
-  }
-
-  async signUp(body: any): Promise<AxiosResponse> {
-    return this.axios.post("/auth/signup/", body);
+  constructor(baseURL: string) {
+    this.axios = axios.create({
+      baseURL: baseURL,
+    });
   }
 }
 
-const API = new APIStub();
-export default API;
+class AuthStub extends APIStub {
+  constructor() {
+    super(process.env.NEXT_PUBLIC_AUTH_BASE_URL ?? "");
+  }
+
+  async signIn(body: any): Promise<AxiosResponse> {
+    return this.axios.post("/auth", body);
+  }
+
+  async signUp(body: any): Promise<AxiosResponse> {
+    return this.axios.post("/auth/signup", body);
+  }
+
+  async sendOTP(body: any): Promise<AxiosResponse> {
+    return this.axios.post("/auth/send-otp", body);
+  }
+}
+
+class CoreStub extends APIStub {
+  constructor() {
+    super(process.env.NEXT_PUBLIC_CORE_BASE_URL ?? "");
+  }
+
+  async createDesires(body: any): Promise<AxiosResponse> {
+    return this.axios.post(`/desires/create`, body);
+  }
+
+  async wantDesires(id: string): Promise<AxiosResponse> {
+    return this.axios.post(`/desires/want/${id}`);
+  }
+
+  async viewItem(id: string): Promise<AxiosResponse> {
+    return this.axios.post(`/view/desire/${id}`);
+  }
+
+  async getTags(): Promise<AxiosResponse> {
+    return this.axios.get(`/tags`);
+  }
+
+  async getActivity(id: string, type: string): Promise<AxiosResponse> {
+    return this.axios.get(`/activity/${id}`, { params: { type: type } });
+  }
+
+  async setAlert(body: any): Promise<AxiosResponse> {
+    return this.axios.post(`/set-alert`, body);
+  }
+
+  async createBid(forDesireId: string, body: any): Promise<AxiosResponse> {
+    return this.axios.post(`/bids/${forDesireId}/create`, body);
+  }
+
+  async acceptBid(bidId: string): Promise<AxiosResponse> {
+    return this.axios.post(`/bids/accept/${bidId}`);
+  }
+
+  async getDesires(params: IFilterParams): Promise<AxiosResponse> {
+    return this.axios.get(`/desires/all`, {
+      params: {
+        "sort-dir": params.sortDir,
+        "filter-by:": params.filterBy,
+        "sort-by": params.sortBy,
+        "per-page": 20,
+        page: params.page,
+      },
+    });
+  }
+
+  async searchDesires(
+    params: IFilterParams,
+    searchString: string
+  ): Promise<AxiosResponse> {
+    return this.axios.get(`/desires/search`, {
+      params: {
+        "search-string": searchString,
+        "sort-dir": params.sortDir,
+        "filter-by:": params.filterBy,
+        "sort-by": params.sortBy,
+        "per-page": 20,
+        page: params.page,
+      },
+    });
+  }
+
+  async getSingleDesire(id: string): Promise<AxiosResponse> {
+    return this.axios.get(`/desires/${id}`);
+  }
+
+  async getAllBids(desireId: string): Promise<AxiosResponse> {
+    return this.axios.get(`/bids/all/${desireId}`);
+  }
+
+  async getSingleBid(bidId: string): Promise<AxiosResponse> {
+    return this.axios.get(`/bids/${bidId}`);
+  }
+
+  async getSingleUser(userId: string): Promise<AxiosResponse> {
+    return this.axios.get(`/users/${userId}`);
+  }
+
+  async updateUser(): Promise<AxiosResponse> {
+    return this.axios.get(`/users`);
+  }
+
+  async getUserDesires(userId: string): Promise<AxiosResponse> {
+    return this.axios.get(`/users/desires/${userId}`);
+  }
+
+  async getUserBids(userId: string): Promise<AxiosResponse> {
+    return this.axios.get(`/users/bids/${userId}`);
+  }
+}
+
+const AuthAPI = new AuthStub();
+const CoreAPI = new CoreStub();
+
+export { AuthAPI, CoreAPI };
