81223c994f5321ae7c9c68305a44cb6bdb58f52e
Yoseph <yosephten@gmail.com>
2024-04-27 22:57:27 +0300

    `refactor`: single offer page

--- app/[desire_id]/[offer_id]/page.tsx
+++ app/[desire_id]/[offer_id]/page.tsx
@@ -1,85 +1,25 @@
-"use client";
-
 import Loader from "@/components/loader";
-import { Button } from "@/components/ui/button";
 import { fetchSingleOffer } from "@/lib/actions/fetch/offer.fetch";
 import { getTitle, getUserId } from "@/lib/helpers";
-import { IOffer, IUser } from "@/lib/types";
-import { CheckIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 import React from "react";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { hasCookie } from "cookies-next";
-import { LoginForm, SignUpForm } from "@/components/profile";
-import { acceptOffer } from "@/lib/actions/act/offer.act";
-import { useToast } from "@/components/ui/use-toast";
-import {
-  fetchUserActivity,
-  fetchUserProfile,
-} from "@/lib/actions/fetch/user.fetch";
+import { fetchUserActivity } from "@/lib/actions/fetch/user.fetch";
+import OfferAcceptUserDetail from "@/components/offer-accept-btn";
 
-export default function SingleOffer() {
-  const [offer, setOffer] = React.useState<IOffer>();
-  const [loading, setLoading] = React.useState(true);
-  const [found, setFound] = React.useState(true);
-  const [modalState, setModalState] = React.useState<boolean>(false);
-  const [accepted, setAccepted] = React.useState(false);
-  const [viewState, setViewState] = React.useState<"SIGNIN" | "SIGNUP">(
-    "SIGNIN"
+export default async function SingleOffer({
+  params,
+}: {
+  params: { desire_id: string; offer_id: string };
+}) {
+  const current_path = `/${params.desire_id}/${params.offer_id}`;
+  const userId = getUserId();
+
+  const offer = await fetchSingleOffer(current_path.split("/")[2]);
+  const accepted = (await fetchUserActivity(userId ?? "", "accepted")).includes(
+    offer?.id ?? ""
   );
-  const current_path = usePathname();
-  const { toast } = useToast();
 
-  React.useLayoutEffect(() => {
-    fetchSingleOffer(current_path.split("/")[2]).then((offer_result) => {
-      if (offer_result) setOffer(offer_result);
-      else setFound(false);
-
-      const userId = getUserId();
-
-      if (userId && offer_result) {
-        fetchUserActivity(userId, "accepted").then((result) => {
-          if (result.includes(offer_result?.id)) setAccepted(true);
-        });
-      }
-
-      setLoading(false);
-    });
-  }, []);
-
-  function handleAcceptOnClick() {
-    if (hasCookie("auth") && offer?.id && !accepted) {
-      acceptOffer(offer?.id).then((result: boolean) => {
-        if (result) setAccepted(true);
-
-        toast({
-          title: (
-            <div className="flex items-center">
-              {result && (
-                <>
-                  <CheckIcon className="mr-2" />
-                  <span className="first-letter:capitalize">
-                    Offer Accepted
-                  </span>
-                </>
-              )}
-              {!result && (
-                <>
-                  <ExclamationTriangleIcon className="mr-2" />
-                  <span className="first-letter:capitalize">
-                    Error Encounterd
-                  </span>
-                </>
-              )}
-            </div>
-          ),
-        });
-      });
-    }
-  }
-
-  return loading ? (
+  return !offer ? (
     <Loader dark />
   ) : (
     <div className="flex flex-col">
@@ -106,87 +46,7 @@ export default function SingleOffer() {
       <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0 mb-4">
         For {offer?.price} Br
       </h2>
-      <Dialog open={modalState} onOpenChange={setModalState}>
-        <div className="flex justify-between mb-4">
-          <DialogTrigger
-            asChild
-            onClick={(e) =>
-              hasCookie("auth") ? e.preventDefault() : setModalState(true)
-            }
-          >
-            <Button
-              onClick={handleAcceptOnClick}
-              variant={accepted ? "secondary" : "default"}
-              disabled={accepted}
-            >
-              {accepted ? (
-                <>
-                  <CheckIcon className="mr-1" />
-                  Accepted
-                </>
-              ) : (
-                <>Accept the Offer</>
-              )}
-            </Button>
-          </DialogTrigger>
-        </div>
-        <DialogContent
-          className="sm:max-w-[825px] flex h-fit w-1/2"
-          onInteractOutside={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <div className="w-1/2 relative">
-            <Image
-              src="/stock-min.jpg"
-              alt="login"
-              fill={true}
-              style={{ objectFit: "cover" }}
-              className="rounded-md"
-            />
-          </div>
-          <div className="w-1/2 flex flex-col justify-between p-2">
-            {viewState === "SIGNIN" && (
-              <LoginForm
-                onSignUpSwitch={() => setViewState("SIGNUP")}
-                onComplete={() => setModalState(false)}
-              />
-            )}
-            {viewState === "SIGNUP" && (
-              <SignUpForm
-                onSignInSwitch={() => setViewState("SIGNIN")}
-                onComplete={() => setModalState(false)}
-              />
-            )}
-          </div>
-        </DialogContent>
-      </Dialog>
-      {accepted && (
-        <>
-          <p className="mb-2 opacity-70 italic">
-            Contact {offer?.bidder} with the below details, to get your item
-          </p>
-          <div className="flex bg-slate-100 p-3 rounded-md outline outline-slate-400 outline-1 mb-4">
-            {offer?.bidder_picture && (
-              <div>
-                <Image
-                  src={offer?.bidder_picture}
-                  height={100}
-                  width={100}
-                  alt="desire"
-                  className="rounded-md mr-5"
-                />
-              </div>
-            )}
-            <div className="flex flex-col">
-              <p className="font-bold">{offer?.bidder}</p>
-              <p>Phone: {offer?.bidder_phone}</p>
-              <p>Email: {offer?.bidder_email}</p>
-              <p>{offer?.bidder_description}</p>
-            </div>
-          </div>
-        </>
-      )}
+      <OfferAcceptUserDetail offer={offer} acceptedProp={accepted} />
     </div>
   );
 }
---
+++ components/offer-accept-btn.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { hasCookie } from "cookies-next";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { CheckIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { LoginForm, SignUpForm } from "./profile";
+import { useToast } from "@/components/ui/use-toast";
+import { acceptOffer } from "@/lib/actions/act/offer.act";
+import { IOffer } from "@/lib/types";
+
+export default function OfferAcceptUserDetail({
+  offer,
+  acceptedProp,
+}: {
+  offer?: IOffer;
+  acceptedProp: boolean;
+}) {
+  const [modalState, setModalState] = React.useState<boolean>(false);
+  const [viewState, setViewState] = React.useState<"SIGNIN" | "SIGNUP">(
+    "SIGNIN"
+  );
+  const [accepted, setAccepted] = React.useState(acceptedProp);
+  const { toast } = useToast();
+
+  function handleAcceptOnClick() {
+    if (hasCookie("auth") && offer?.id && !accepted) {
+      acceptOffer(offer?.id).then((result: boolean) => {
+        if (result) setAccepted(true);
+
+        toast({
+          title: (
+            <div className="flex items-center">
+              {result && (
+                <>
+                  <CheckIcon className="mr-2" />
+                  <span className="first-letter:capitalize">
+                    Offer Accepted
+                  </span>
+                </>
+              )}
+              {!result && (
+                <>
+                  <ExclamationTriangleIcon className="mr-2" />
+                  <span className="first-letter:capitalize">
+                    Error Encounterd
+                  </span>
+                </>
+              )}
+            </div>
+          ),
+        });
+      });
+    }
+  }
+  return (
+    <>
+      <Dialog open={modalState} onOpenChange={setModalState}>
+        <div className="flex justify-between mb-4">
+          <DialogTrigger
+            asChild
+            onClick={(e) =>
+              hasCookie("auth") ? e.preventDefault() : setModalState(true)
+            }
+          >
+            <Button
+              onClick={handleAcceptOnClick}
+              variant={accepted ? "secondary" : "default"}
+              disabled={accepted}
+            >
+              {accepted ? (
+                <>
+                  <CheckIcon className="mr-1" />
+                  Accepted
+                </>
+              ) : (
+                <>Accept the Offer</>
+              )}
+            </Button>
+          </DialogTrigger>
+        </div>
+        <DialogContent
+          className="sm:max-w-[825px] flex h-fit w-1/2"
+          onInteractOutside={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="w-1/2 relative">
+            <Image
+              src="/stock-min.jpg"
+              alt="login"
+              fill={true}
+              style={{ objectFit: "cover" }}
+              className="rounded-md"
+            />
+          </div>
+          <div className="w-1/2 flex flex-col justify-between p-2">
+            {viewState === "SIGNIN" && (
+              <LoginForm
+                onSignUpSwitch={() => setViewState("SIGNUP")}
+                onComplete={() => setModalState(false)}
+              />
+            )}
+            {viewState === "SIGNUP" && (
+              <SignUpForm
+                onSignInSwitch={() => setViewState("SIGNIN")}
+                onComplete={() => setModalState(false)}
+              />
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
+      {accepted && (
+        <>
+          <p className="mb-2 opacity-70 italic">
+            Contact {offer?.bidder} with the below details, to get your item
+          </p>
+          <div className="flex bg-slate-100 p-3 rounded-md outline outline-slate-400 outline-1 mb-4">
+            {offer?.bidder_picture && (
+              <div>
+                <Image
+                  src={offer?.bidder_picture}
+                  height={100}
+                  width={100}
+                  alt="desire"
+                  className="rounded-md mr-5"
+                />
+              </div>
+            )}
+            <div className="flex flex-col">
+              <p className="font-bold">{offer?.bidder}</p>
+              <p>Phone: {offer?.bidder_phone}</p>
+              <p>Email: {offer?.bidder_email}</p>
+              <p>{offer?.bidder_description}</p>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
